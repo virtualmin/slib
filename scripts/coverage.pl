f10a5ae8a01d31f670c53d9736b5567e67f7aad7
Joe Cooper <swelljoe@gmail.com>
2017-08-12 09:16:00 -0500

    coverage.pl to show unit test coverage

---
+++ scripts/coverage.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+use List::MoreUtils qw(uniq);
+
+my $file = $ARGV[0];
+my $testdir = 't';
+
+open my $handle, '<', $file;
+chomp(my @lines = <$handle>);
+close $handle;
+
+use Data::Dumper;
+#print Dumper(@lines);
+
+# Get a list of functions that ideally would be tested
+my @funcs;
+for( @lines ) {
+	my $func = $1 if m/^([a-zA-Z0-9\_]+)\W*\(\)\W*{/;
+	push @funcs, $func if $func;
+}
+
+#print Dumper(@funcs);
+
+# Get a list of functions that have a test that calls them
+my @tests = <./t/*>;
+my @tested;
+for my $test (@tests) {
+	open my $handle, '<', $test;
+	chomp(my @lines = <$handle>);
+	close $handle;
+	for ( @lines ) {
+		my $func = $1 if m/run\.sh\W'([a-zA-Z0-9\_]+)\W.*/;
+		push @tested, $func if $func;
+	}
+}
+
+# Figure out what's missing
+my @missing;
+for my $f (@funcs) {
+	push @missing, $f unless grep( /$f/, @tested );
+}
+
+my $coverage = sprintf '%.2f', $#tested / $#funcs * 100;
+say "Test Coverage: $coverage%";
+say "Untested functions:";
+for my $f (@missing) {
+	say $f;
+}
+
